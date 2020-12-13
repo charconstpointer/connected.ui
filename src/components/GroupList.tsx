5bee8f3a2e98708006d3cd4a8c9f4c8bfb43aee3
charconstpointer <attwistingnether@gmail.com>
2020-12-13 02:01:56 +0100

    grouplist

--- src/components/GroupList.tsx
+++ src/components/GroupList.tsx
@@ -12,26 +12,29 @@ const GroupList = (props: any) => {
     history.push(`/groups/${groupId}`)
   }
   return (
-    <div className="list-group mt-5 shadow-sm p-3 mb-5 bg-white rounded">
-      <div className="input-group mb-3">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="basic-addon1">🔍</span>
+    <div className="container">
+      <div className="row list-group mt-5 shadow-sm p-3 mb-5 bg-white rounded">
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">🔍</span>
+          </div>
+          <input type="text" className="form-control" onChange={handleSearchChange} placeholder="Search by keywords or tags" aria-label="Search"
+            aria-describedby="basic-addon1" />
         </div>
-        <input type="text" className="form-control" onChange={handleSearchChange} placeholder="Search by keywords or tags" aria-label="Search"
-          aria-describedby="basic-addon1" />
-      </div>
 
-      <ul className="list-group list-group-flush">
-        {props.groups.filter((g: GroupModel) => g.name.includes(search)).map((g: GroupModel) => {
-          return (
-            <li className="list-group-item">
-              <p>{g.name}</p>
-              <button className="btn btn-primary" onClick={() => handleJoinGroup(g.id)}  >Join group</button>
-            </li>
-          )
-        })}
-      </ul>
-    </div >
+        <ul className="list-group list-group-flush">
+          {props.groups.filter((g: GroupModel) => g.name.includes(search)).map((g: GroupModel) => {
+            return (
+              <li className="list-group-item">
+                <p>{g.name}</p>
+                <button className="btn btn-primary" onClick={() => handleJoinGroup(g.id)}  >Join group</button>
+              </li>
+            )
+          })}
+        </ul>
+      </div >
+    </div>
+
   )
 }
 
