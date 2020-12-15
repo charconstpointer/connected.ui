80a40dcacead973c0b104378bd8428da3eb8caa5
charconstpointer <attwistingnether@gmail.com>
2020-12-16 00:01:42 +0100

    styles? styles

--- src/components/Groups.tsx
+++ src/components/Groups.tsx
@@ -1,28 +1,22 @@
 import React, { useEffect, useState } from "react";
 import GroupModel, { fromJson } from "../models/GroupModel";
 import CreateNewGroup from '../requests/CreateNewGroup'
+import { Post as POST } from "../utils/api";
 import GroupList from './GroupList'
 
 export const Groups = () => {
-  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [tags, setTags] = useState<string[]>([]);
   const [groupName, setGroupName] = useState("");
   const [description, setDescription] = useState("");
   const [groups, setGroups] = useState<GroupModel[]>([]);
   const [fetched, setFetched] = useState(false);
 
   const fetchGroups = async () => {
-    console.log('uef')
-
     const response = await fetch("https://localhost:5001/groups")
-    console.log(response);
-
     const data = await response.json();
-    console.log(data);
-
     const gs = data.map((gr: any) => fromJson(gr))
     setGroups(g => [...gs])
     setFetched(true)
-    console.log(groups)
   }
   useEffect(() => {
     fetchGroups()
@@ -35,18 +29,16 @@ export const Groups = () => {
   const handleDescriptionChange = (ev: any) => {
     setDescription(ev.target.value)
   }
+  const handleTagsChange = (ev: any) => {
+    const value = ev.target.value as string;
+    const tokens = value.split(",").map(x => x.trim());
+    setTags(tokens);
+    console.log(tokens)
+  }
   const handleCreateGroup = async () => {
     console.log(groupName, description)
-    const request = new CreateNewGroup(groupName, description);
-    const response = await fetch("https://localhost:5001/groups", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify(request)
-    })
-
+    const request = new CreateNewGroup(groupName, description, tags);
+    const response = await POST("https://localhost:5001/groups", request);
     if (response.status !== 500) {
       fetchGroups()
     }
@@ -54,6 +46,7 @@ export const Groups = () => {
 
   return (
     <div>
+      <h1 className="display-3 pb-5 pt-5">Available groups</h1>
       {!fetched ?
         <div className="spinner-border" role="status">
           <span className="sr-only">Loading...</span>
@@ -64,21 +57,26 @@ export const Groups = () => {
 
       }
 
-
       <div className="create-group mt-5 shadow-sm p-3 mb-5 bg-white rounded">
         <p className="lead">Nothing that suits your interests? Create your own group!</p>
         <div className="container mb-5 mt-5">
           <div className="input-group mb-3">
             <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroup-sizing-default">Group name</span>
+              <span className="input-group-text" id="inputGroup-sizing-default">Group name 💁🏽‍♀️</span>
             </div>
             <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={handleGroupNameChange} />
           </div>
           <div className="input-group mb-3">
             <div className="input-group-prepend">
-              <span className="input-group-text" id="inputGroup-sizing-default">Description</span>
+              <span className="input-group-text" id="inputGroup-sizing-default">Description 📝</span>
             </div>
             <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={handleDescriptionChange} />
+          </div>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-default">Tags 🏷</span>
+            </div>
+            <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" onChange={handleTagsChange} />
           </div>
           <a onClick={handleCreateGroup} className="btn btn-primary btn-block" href="#">Create</a>
         </div>
--- src/requests/createNewGroup.tsx
+++ src/requests/createNewGroup.tsx
@@ -2,9 +2,9 @@ export default class CreateNewGroup {
     name: string
     description: string
     tags: Array<string>
-    constructor(groupName: string, description: string) {
+    constructor(groupName: string, description: string, tags: string[]) {
         this.name = groupName;
         this.description = description;
-        this.tags = [groupName, description]
+        this.tags = [...tags, groupName, description]
     }
 }
